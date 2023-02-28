e3573fe1e2eb8f44b70beaebd05e50f8769b8c3e
Ito Tatsuo <pleinair1102@gmail.com>
2023-03-01 01:04:29 +0900

    post update

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,106 @@ import express from 'express'
 import * as line from '@line/bot-sdk';
 
 const config = {
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN as string,
-    channelSecret: process.env.CHANNEL_SECRET as string,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN as string,
+  channelSecret: process.env.CHANNEL_SECRET as string,
 };
 const app: express.Express = express()
 app.post('/webhook', line.middleware(config), (req, res) => {
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result));
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
 });
 
 const client = new line.Client(config);
 function handleEvent(event: line.WebhookEvent) {
-    if (event.type === 'postback') {
+  if (event.type === 'postback') {
 
-        console.log(`PostBack:${event.postback.data} User:${event.source.userId}`);
-        return client.replyMessage(event.replyToken, {
-            type: 'text',
-            text: `user:${event.source.userId} yoyaku`
-        });
+    console.log(`PostBack:${event.postback.data} User:${event.source.userId} params:${event.postback.params}`);
+    if ((event.postback.params as any).action === 'yoyakusuru') {
+      return client.replyMessage(event.replyToken, {
+        type: 'flex',
+        altText: "yoyaku",
+        contents: {
+          "type": "carousel",
+          "contents": [
+            {
+              "type": "bubble",
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "指名なし"
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "postback",
+                      "label": "OK",
+                      "data": "parson=none"
+                    }
+                  }
+                ]
+              },
+              "styles": {
+                "header": {
+                  "separator": false
+                }
+              }
+            },
+            {
+              "type": "bubble",
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Aさん"
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "postback",
+                      "label": "OK",
+                      "data": "parson=A"
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      });
     }
-    if (event.type !== 'message' || event.message.type !== 'text') {
-        return Promise.resolve(null);
+
+    if (event.postback.params) {
+
     }
+  }
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
 
-    console.log(`user:${event.source.userId} message:${event.message.text}`);
+  console.log(`user:${event.source.userId} message:${event.message.text}`);
 
-    return client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: `user:${event.source.userId} message:${event.message.text} らしいよ`
-    });
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: `user:${event.source.userId} message:${event.message.text} らしいよ`
+  });
 }
 
 console.log("Start!!!");
